9574257f9537e6d43f0bc666cdf9380f29976bda
Shi Chen <45330366+ShiChenAI@users.noreply.github.com>
2018-12-18 10:31:22 +0900

    Add file convertion.

--- download_files.py
+++ download_files.py
@@ -1,13 +1,23 @@
 import os
-import urllib
+import urllib.request
 import os
 import sys
+from PIL import Image 
 
 data_path = './data/'
 
 def _progress(block_num, block_size, total_size):
-    sys.stdout.write('\r>> Downloading %.1f%%' % (float(block_num * block_size) / float(total_size) * 100.0))
+    float_process = float(block_num * block_size) / float(total_size) * 100.0
+    if float_process > 100:
+        float_process = 100
+    sys.stdout.write('\r>> Downloading %.1f%%' % (float_process))
     sys.stdout.flush()
+
+
+def get_file_name(file_path):
+    (filepath,tempfilename) = os.path.split(file_path)
+    (filename,extension) = os.path.splitext(tempfilename)
+    return filename
 
 
 def download(url_file_name, targets):
@@ -16,6 +26,7 @@ def download(url_file_name, targets):
         while line:
             items = line.split('\t')
             file_type = items[0]
+            print(file_type)
             url = items[1]
             if file_type not in targets:
                 continue
@@ -27,6 +38,10 @@ def download(url_file_name, targets):
             file_name = data_path + file_type + '/' + url.split('/')[-1]
             print(file_name)
             urllib.request.urlretrieve(url, file_name, _progress)
+            im = Image.open(file_name)
+            im = im.convert('RGB')
+            im.save(data_path + file_type + '/' + get_file_name(file_name) + '.jpg')
+            os.remove(file_name)
 
 
 def main():
